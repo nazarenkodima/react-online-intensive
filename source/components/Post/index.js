9d1deb37f0f93649cdaa0de2addde2ef2eadd6c9
Dmytro Nazarenko <dmamyk@gmail.com>
2018-11-24 19:33:34 +0200

    add context to Posts component

--- source/components/Post/index.js
+++ source/components/Post/index.js
@@ -16,16 +16,8 @@ export default class Post extends Component {
 
         return (
             <section className = { Styles.post }>
-<<<<<<< HEAD
-                <img src = { avatar } />
-                <a> {currentUserFirstName} {currentUserLastName} </a>
-||||||| merged common ancestors
-                <img src = { avatar } />
-                <a> {`${currentUserFirstName} ${currentUserLastName}`} </a>
-=======
                 <img src = { context.avatar } />
                 <a> {`${context.currentUserFirstName} ${context.currentUserLastName}`} </a>
->>>>>>> react-online-lesson-4
                 <time> {moment().format('MMMM D h:mm:ss a')} </time>
                 <p> Hello! </p>
             </section>
